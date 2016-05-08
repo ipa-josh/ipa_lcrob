91ffc8072ca12ea5c0d970ec79ddeacfc5842cbb
Joshua Hampp <joshua.hampp@ipa.fraunhofer.de>
2016-05-08 09:18:13 +0200

    update

--- fb_eval/src/compare_poses.py
+++ fb_eval/src/compare_poses.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import rosbag, os, math, argparse, rospy
+import rosbag, os, math, argparse, rospy, sys
 from std_msgs.msg import Int32, String
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import PoseStamped, Pose2D
@@ -66,51 +66,90 @@ try:
 				poses_gt[msg.header.stamp] = p
 				last_gt=p
         elif topic==topic_scan:
+            #print msg.header.stamp.to_sec(), t.to_sec()
             scans[msg.header.stamp] = msg
             
-        if len(poses_gt)>800: break
+        #if len(poses_gt)>800: break
 finally:
     bagin.close()
     
+print >> sys.stderr, "got ", len(poses_gt)
 
 def get_scan(ts):
 	global scans
 	ts_m = min(scans, key=lambda x:abs(x-ts))
-	if abs(ts_m-ts)>rospy.Duration.from_sec(0.1):
+	#print "found a scan ", ts_m.to_sec(), ts.to_sec(), (ts_m-ts).to_sec()
+	if abs(ts_m-ts)>rospy.Duration.from_sec(0.05):
 		return None
 	return scans[ts_m]
 
+
+per_l=-1
+n_l=0
+results={}
+Ds={}
 for ts in sorted(poses_gt):
-	s1 = get_scan(ts)
+	n_l += 1
+	
+	s1 = get_scan(ts+rospy.Duration(0.05))
 	if s1==None: continue
+	
+	per = 100*n_l/len(poses_gt)
+	if per_l!=per:
+		per_l=per
+		print >> sys.stderr, per_l,"%"
 	
 	for ts2 in sorted(poses_gt):
 		if rospy.is_shutdown(): exit()
 		
-		if ts2-ts<=rospy.Duration.from_sec(0.2): continue
+		if ts2-ts<=rospy.Duration.from_sec(0.5): continue
 		
 		D = np.dot(transformations.inverse_matrix(poses_gt[ts]),poses_gt[ts2])
-		if transformations.vector_norm(D[:3, 3])<0.1: continue
+		dist = (transformations.vector_norm(D[:3, 3]), abs(transformations.euler_from_matrix(D, 'rxyz')[2]))
+		if (dist[0]<0.1 and dist[1]<0.05) or dist[0]>0.3 or dist[1]>0.8: continue
 		
-		s2 = get_scan(ts2)
+		s2 = get_scan(ts2+rospy.Duration(0.05))
 		
 		if s2==None: continue
 		
 		recv_pose = None 
 		
-		print D
-		while recv_pose==None and not rospy.is_shutdown():
-			pub1.publish(s1)
-			pub2.publish(s2)
-		
-			rospy.sleep(0.5)
+		#print D
+		if s1.header.stamp in Ds and s2.header.stamp in Ds[s1.header.stamp]:
+			recv_pose = Ds[s1.header.stamp][s2.header.stamp]
+		else:
+			while recv_pose==None and not rospy.is_shutdown():
+				pub1.publish(s1)
+				pub2.publish(s2)
+			
+				for x in xrange(50):
+					if recv_pose!=None or rospy.is_shutdown(): break
+					rospy.sleep(0.01)
 			
 		Di = transformations.inverse_matrix(D)
-		print "gt  ", transformations.euler_from_matrix(D, 'rxyz'), D[:3, 3]
-		print "gtI ", transformations.euler_from_matrix(Di, 'rxyz'), Di[:3, 3]
-		print "ms ", recv_pose
+		#print "gt  ", transformations.euler_from_matrix(D, 'rxyz'), D[:3, 3]
+		#print "gtI ", transformations.euler_from_matrix(Di, 'rxyz'), Di[:3, 3]
+		#print "ms ", recv_pose
 		
 		d = (D[:3, 3][0]-recv_pose.y, D[:3, 3][1]+recv_pose.x)
 		a = transformations.euler_from_matrix(D, 'rxyz')[2]+recv_pose.theta
 		
-		print "distance ", transformations.vector_norm(d), abs(a), transformations.vector_norm(D[:3, 3]), abs(transformations.euler_from_matrix(D, 'rxyz')[2])
+		#print "distance ", transformations.vector_norm(d), abs(a), transformations.vector_norm(D[:3, 3]), abs(transformations.euler_from_matrix(D, 'rxyz')[2])
+		
+		r = (transformations.vector_norm(d), abs(a), transformations.vector_norm(D[:3, 3]), abs(transformations.euler_from_matrix(D, 'rxyz')[2]))
+		if not s1.header.stamp in results:
+			results[s1.header.stamp] ={}
+			Ds[s1.header.stamp] ={}
+			results[s1.header.stamp][s2.header.stamp] = r
+			Ds[s1.header.stamp][s2.header.stamp] = recv_pose
+		elif not s2.header.stamp in results[s1.header.stamp]:
+			results[s1.header.stamp][s2.header.stamp] = r
+			Ds[s1.header.stamp][s2.header.stamp] = recv_pose
+		elif results[s1.header.stamp][s2.header.stamp][0]+results[s1.header.stamp][s2.header.stamp][1] > r[0]+r[1]:
+			results[s1.header.stamp][s2.header.stamp] = r
+			
+		#print results[s1.header.stamp][s2.header.stamp]
+
+for k1 in results:
+	for k2 in results[k1]:
+		print str(results[k1][k2])[1:-1]
--- fb_eval/src/generate_files.py
+++ fb_eval/src/generate_files.py
@@ -10,24 +10,29 @@ NOISES=[0.0, 0.025, 0.05, 0.1]
 #add extension
 for i in xrange(len(FILES)):
 	FILES[i] = FILES[i]+".bag"
+	
+def comp(noise, dev, angle):
+	odom_noise = noise
+	img_noise  = 2*noise
+	min_angle = -angle
+	max_angle =  angle
+	
+	fn_out_desc = fn.replace(".bag",".desc."+str(dev)+"."+str(angle)+"."+str(noise)+".txt")
+	fn_out_bag  = fn.replace(".bag",".out."+str(dev)+"."+str(angle)+"."+str(noise)+".bag")
+	if os.path.isfile(fn_out_desc) and os.path.isfile(fn_out_bag): return
+	
+	r = os.system("rosrun fb_eval simulate_sensor.py "+fn+" --deviation "+str(dev)+" --min_angle "+str(min_angle)+" --max_angle "+str(max_angle)+" --odom_noise "+str(odom_noise)+" --img_noise "+str(img_noise))
+	if r!=0:
+		print "stopping because of error"
+		exit()
+	
+	os.system("mv "+fn.replace(".bag",".out.bag")+ " "+fn_out_bag)
+	os.system("mv "+fn.replace(".bag",".desc.txt")+" "+fn_out_desc)
 
 for fn in FILES:
 	for dev in DEV:
-		for angle in ANGLES:
-			min_angle = -angle
-			max_angle =  angle
-			for noise in NOISES:
-				odom_noise = noise
-				img_noise  = 2*noise
-				
-				fn_out_desc = fn.replace(".bag",".desc."+str(dev)+"."+str(angle)+"."+str(noise)+".txt")
-				fn_out_bag  = fn.replace(".bag",".out."+str(dev)+"."+str(angle)+"."+str(noise)+".bag")
-				if os.path.isfile(fn_out_desc) and os.path.isfile(fn_out_bag): continue
-				
-				r = os.system("rosrun fb_eval simulate_sensor.py "+fn+" --deviation "+str(dev)+" --min_angle "+str(min_angle)+" --max_angle "+str(max_angle)+" --odom_noise "+str(odom_noise)+" --img_noise "+str(img_noise))
-				if r!=0:
-					print "stopping because of error"
-					exit()
-				
-				os.system("mv "+fn.replace(".bag",".out.bag")+ " "+fn_out_bag)
-				os.system("mv "+fn.replace(".bag",".desc.txt")+" "+fn_out_desc)
+		comp(0.0, dev, 100.)
+	for angle in ANGLES:
+		comp(0.0, 0.0, angle)
+	for noise in NOISES:
+		comp(noise, 0.0, 100.)
